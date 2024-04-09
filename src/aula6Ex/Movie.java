b7efbbe8ace60518465da96cdaaa171bd1aa1ec9
jonas mendes <jonas.jsm2903@gmail.com>
2024-04-09 08:40:04 -0300

    exercicio Aula6Ex

---
+++ src/aula6Ex/Movie.java
@@ -0,0 +1,10 @@
+package aula6;
+
+public class Movie {
+
+    public String name;
+    public int year;
+    public boolean isPlan;
+    public double validation;
+    public int allValidation;
+}
---
+++ src/aula6Ex/Start.java
@@ -0,0 +1,7 @@
+package aula6;
+
+public class Start {
+    public static void main(String[] args) {
+
+    }
+}
